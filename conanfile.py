fc3fa359b44ffa83139fd40eaefc757cb626b251
Mark Jan van Kampen <mjvk@allseas.com>
2019-08-19 16:09:22 +0200

    Changes to use original top-level makefile
    On some systems this gave errors (similar to https://www.postgresql
    .org/message-id/15407-7b64e5de97dedc5e%40postgresql.org . This chan-
    ge might involve more build time (to be checked), but it should sol-
    ve errors such as https://github.com/bincrafters/community/issues/
    912

--- conanfile.py
+++ conanfile.py
@@ -84,13 +84,7 @@ class LibpqConan(ConanFile):
             cmake.build()
         else:
             autotools = self._configure_autotools()
-            with tools.chdir(os.path.join(self._source_subfolder, "src", "common")):
-                autotools.make()
-            with tools.chdir(os.path.join(self._source_subfolder, "src", "include")):
-                autotools.make()
-            with tools.chdir(os.path.join(self._source_subfolder, "src", "interfaces", "libpq")):
-                autotools.make()
-            with tools.chdir(os.path.join(self._source_subfolder, "src", "bin", "pg_config")):
+            with tools.chdir(self._source_subfolder):
                 autotools.make()
 
     def package(self):
@@ -100,13 +94,7 @@ class LibpqConan(ConanFile):
             cmake.install()
         else:
             autotools = self._configure_autotools()
-            with tools.chdir(os.path.join(self._source_subfolder, "src", "common")):
-                autotools.install()
-            with tools.chdir(os.path.join(self._source_subfolder, "src", "include")):
-                autotools.install()
-            with tools.chdir(os.path.join(self._source_subfolder, "src", "interfaces", "libpq")):
-                autotools.install()
-            with tools.chdir(os.path.join(self._source_subfolder, "src", "bin", "pg_config")):
+            with tools.chdir(self._source_subfolder):
                 autotools.install()
             tools.rmdir(os.path.join(self.package_folder, "include", "postgresql", "server"))
             self.copy(pattern="*.h", dst=os.path.join("include", "catalog"), src=os.path.join(self._source_subfolder, "src", "include", "catalog"))
